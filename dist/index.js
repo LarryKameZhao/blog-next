760cf0ed172624a6e21d92ceefbad76573e97645
LarryKameZhao <larryzhao1994@outlook.com>
2020-09-02 08:01:45 +0800

    typeorm

--- dist/index.js
+++ dist/index.js
@@ -10,16 +10,31 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
+var _Post = require("./entity/Post");
+
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
+    var p, posts;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            console.log(connection);
+            p = new _Post.Post();
+            p.title = 'first post';
+            p.content = '第一篇文章';
+            _context.next = 5;
+            return connection.manager.save(p);
+
+          case 5:
+            _context.next = 7;
+            return connection.manager.find(_Post.Post);
+
+          case 7:
+            posts = _context.sent;
+            console.log(posts);
             connection.close();
 
-          case 2:
+          case 10:
           case "end":
             return _context.stop();
         }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+import { Post } from './entity/Post';
 
 createConnection()
   .then(async (connection) => {
-    console.log(connection);
+    const p = new Post();
+    p.title = 'first post';
+    p.content = '第一篇文章';
+    await connection.manager.save(p);
+    const posts = await connection.manager.find(Post);
+    console.log(posts);
     connection.close();
   })
   .catch((error) => console.log(error));
